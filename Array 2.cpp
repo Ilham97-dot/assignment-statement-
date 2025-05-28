7bfd9e911d6fe90b9cc46b954878b82cef5281b4
Ilham97-dot <ilhamhardiansyah994@gmail.com>
2025-05-28 19:46:33 +0700

    Array

---
+++ Array 2.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+main () { 
+int I;
+int A[5] = {12,13,14,15,16};
+for (I=0; I<=4; I++) { 
+printf
+("%3i",A[I]);
+}
+} 
---
+++ Array 3.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h> 
+#define n 5
+main ()
+{
+int I;
+int A[n] = {1,2,90,98,55};
+for (I=0; I<=4; I++)
+{
+printf ("%3i",A[I]);
+}
+} 
+
